9ad7612da4a9e4a53534c2e67ef2d92728387169
Smlight <imzy140@gmail.com>
2017-04-15 23:16:57 +0800

    update

--- AirMatters/AirMatters/settings.py
+++ AirMatters/AirMatters/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'weather',
             'USER': 'root',
-            'PASSWORD': 'admin888',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
             'PORT': '3306',
         }
--- AirMatters/weather/views.py
+++ AirMatters/weather/views.py
@@ -8,7 +8,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from weather.models import Beijing, Shanghai, Guangzhou, Shenzhen, Hangzhou, Tianjin, Chengdu, Nanjing, Xian, Wuhan
 from weather.models import Realtime, Forecast
-import datetime
 
 
 def current_datetime(request):
@@ -26,47 +25,50 @@ import requests
 he_key = "88cef94b40a4461ea933dfc44c41f3a2"  # 和风天气API key
 he_str = "https://free-api.heweather.com/v5/weather"  # 和风天气API 接口
 
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 def default(request):
     city_str = 'beijing'
+
+    flag = 0  # no need to update
     try:
         pre = Realtime.objects.get(city=city_str)
     except:
-        pre = Realtime(city=city_str)
-        pre.time = datetime.datetime.now() + datetime.timedelta(hours=-2)
-    now = pre
-    webmain = str(datetime.datetime.now())
-    if pre.time + datetime.timedelta(hours=1) < datetime.datetime.now():
-        if pre.id:
-            pre.delete()
+        flag = 1  # no data before
+    if flag == 0 and pre.time + timedelta(hours=1) < timezone.now():
+        pre.delete()
+        flag = 2  # data too old
+
+    if flag == 0:
+        now = pre
+    else:
         payload = {'city': city_str, 'key': he_key}
-        # try:
-
-        r = requests.get(he_str, params=payload)
-        J = r.json()
-        J = J[u'HeWeather5'][0]
-        if J[u'status'] != u'ok':
+        try:
+            r = requests.get(he_str, params=payload)
+            J = r.json()
+            J = J[u'HeWeather5'][0]
+            if J[u'status'] != u'ok':
+                pass
+            now = Realtime(city=city_str)
+            now.time = J[u'basic'][u'update'][u'loc']
+            Jnow = J[u'now']
+            now.cond = Jnow[u'cond'][u'txt']
+            now.hum = int(Jnow[u'hum'])
+            now.pcpn = int(Jnow[u'pcpn'])
+            now.pres = int(Jnow[u'pres'])
+            now.tmp = int(Jnow[u'tmp'])
+            now.vis = int(Jnow[u'vis'])
+            now.wind_dir = Jnow[u'wind'][u'dir']
+            now.wind_sc = Jnow[u'wind'][u'sc']
+            Jaqi = J[u'aqi'][u'city']
+            now.aqi = int(Jaqi[u'aqi'])
+            now.aqi_str = Jaqi[u'qlty']
+            now.pm25 = int(Jaqi[u'pm25'])
+            now.suggestion = J[u'suggestion']
+            now.save()
+            webmain = str(J)
+        except:
             pass
-        now = Realtime(city=city_str)
-        now.time = J[u'basic'][u'update'][u'loc']
-        Jnow = J[u'now']
-        now.cond = Jnow[u'cond'][u'txt']
-        now.hum = int(Jnow[u'hum'])
-        now.pcpn = int(Jnow[u'pcpn'])
-        now.pres = int(Jnow[u'pres'])
-        now.tmp = int(Jnow[u'tmp'])
-        now.vis = int(Jnow[u'vis'])
-        now.wind_dir = Jnow[u'wind'][u'dir']
-        now.wind_sc = Jnow[u'wind'][u'sc']
-        Jaqi = J[u'aqi'][u'city']
-        now.aqi = int(Jaqi[u'aqi'])
-        now.aqi_str = Jaqi[u'qlty']
-        now.pm25 = int(Jaqi[u'pm25'])
-        now.suggestion = J[u'suggestion']
-        now.save()
-        webmain = str(J)
 
-        # except:
-        #     webmain = u'NetWork FatalError!'
-
-    return render(request, 'realtime.html', {'webmain': webmain, 'now': now})
+    return render(request, 'realtime.html', {'now': now})
