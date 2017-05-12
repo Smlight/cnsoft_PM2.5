490ea4af85a6ec0a881b8d7dd6b9cfb7f9127298
Smlight <imzy140@gmail.com>
2017-05-12 15:54:12 +0800

    setting improved

--- AirMatters/AirMatters/settings.py
+++ AirMatters/AirMatters/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'weather',
             'USER': 'root',
-            'PASSWORD': '',
+            'PASSWORD': '123456',
             'HOST': '127.0.0.1',
             'PORT': '3306',
         }
